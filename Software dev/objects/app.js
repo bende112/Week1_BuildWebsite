d5906fd843c94b0fd00e9b2d7c0bc2b8246b9e2f
Bende <bende.nku@yahoo.com>
2023-12-05 10:01:39 +0000

    objects of weather

---
+++ Software dev/objects/app.js
@@ -0,0 +1,29 @@
+const myName = "Bende";
+const myAge = 43;
+const myHappiness = true;
+const myKids = ["Teagan", "Wonda", "Betty"];
+
+const myStuffs = {name: "Bende", age: 43, myHappiness: true};
+
+console.log(myStuffs)
+
+myStuffs.favouriteFood = "Steak";
+console.log(myStuffs);
+myStuffs["name"];
+myStuffs["age"]
+myStuffs.name;
+myStuffs.age;
+
+
+const weather = {
+    summer:"warmWeather, happyPeople, sunny",
+    spring: "newFlower",
+    winter: "snow, cold",
+}
+
+
+console.log(weather.spring);
+
+weather.autumn = "colorfullTress, decreasingTemperature"
+console.table(weather)
+
